86da53710df748e52f0bc2ebb90525e7d738e508
keta-dev <preyedan_5@yahoo.com>
2022-06-14 21:26:53 +0100

    weather push

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -1,86 +1,48 @@
-//require the express application framework
-const express = require('express');
-//initiate an app instance from express library
-const app = express();
-//setup the port
-const PORT = 8080; //port doesn't have a special name but it is just a window to test the applicaton locally.
-//make the endpoint object to hold the data coming from the client side
-let projectData = {};
-//use the express middleware for turning body and external input and coming data to be stingfied from json
-app.use(express.json());//parse the json format to normal string;
-app.use(express.urlencoded({ extended: false }));//is a body parser for html post form
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
 
-//use the website folder as the initial path for our files
-app.use(express.static("website"));
-//setup cors module to enable server to listen to other http/s requests out of the server domain/port/localhost.
-const cors = require('cors');
-//use the cors middleware
+// Require Express to run server and routes
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// const fetch = require('node-fetch');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
 app.use(cors());
 
-app.post('/add', async (req, res) => {
-    const info = await req.body;
-    projectData = info;
-    res.send(projectData);
-});
+// Initialize the main project folder
+app.use(express.static("website"));
 
-app.get("/all", async (req, res) => {
-    if(projectData){
-        res.send(projectData);
-    }
-});
+// post route
+app.post("/add", getPost);
 
-//listening for the server through the express.listen and which gives us confirmation that the server is working properly
-// app.listen(port, _=> console.log(`listening on port ${port}`));
-app.listen(PORT, () => {
-  console.log(`listening on Port http://localhost:${PORT}/`);
-})
+// function for POST request '/add'
+function getPost(req, res) {
+  projectData.temp = req.body.temp;
+  projectData.date = req.body.date;
+  projectData.content = req.body.content;
+  res.status(200).send(projectData);
+}
 
+// get route
+app.get("/all", getInfo);
 
-// // Setup empty JS object to act as endpoint for all routes
-// projectData = {};
-
-// // Require Express to run server and routes
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// // const fetch = require('node-fetch');
-
-// // Start up an instance of app
-// const app = express();
-
-// /* Middleware*/
-// //Here we are configuring express to use body-parser as middle-ware.
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
-// // Cors for cross origin allowance
-// app.use(cors());
-
-// // Initialize the main project folder
-// app.use(express.static('website'));
-
-// // To get the /all route
+function getInfo(req, res) {
+  res.status(200).send(projectData);
+};
+// To get the /all route
 // const getAllData = (req, res) => res.status(200).send(projectData);
 
-// // get route
-// app.get('/all', getAllData);
-
-// // function for POST request '/add'
-// function getPost (req, res) {
-//   res.status(200).send(req.body);
-//   newEntries = {
-//     temp: req.body.temp,
-//     content: req.body.content,
-//     date: req.body.date
-//   }
-//   projectData.push(newEntries)
-// }
-
-// // post route
-// app.post('/add', getPost);
-
-// // Setup Server
-// const PORT = 8080;
-// app.listen(PORT, () => {
-//   console.log(`listening on Port http://localhost:${PORT}/`);
-// })
+// Setup Server
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`listening on Port http://localhost:${PORT}/`);
+});
--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,108 +1,74 @@
 /* Global Variables */
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const apiKey = "&appid=220a3706ff5c931509ff38caacd19574&units=imperial";
 
-// API call here
-const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
-const apiKey = "&appid=7b8065cc7957999c5c5d6cd2d741b8f2&units=imperial";
-
-// from here you input the zip and feeling values
-const zipInput = document.getElementById("zip");
-const myInput = document.getElementsByClassName("myInput");
-const btn = document.getElementById("generate");
+const generate = document.getElementById("generate");
 const form = document.getElementById("form");
 
-btn.addEventListener('click', Generate);
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+// generate.addEventListener("click", performAction);
+generate.addEventListener("click", Generate);
 
 function Generate(e) {
   e.preventDefault();
-  console.log('clicked');
 
-  const zip = document.getElementById('zip').value;
-  const feelings = document.getElementById('feelings').value;
+  const zipID = document.getElementById("zip").value;
+  const content = document.getElementById("feelings").value;
 
-  if (zip !== '') {
-    getWeather(baseURL, zip, apiKey)
-    .then((data) => {
-      postData('/add', {date: newDate, temp: tempConverter(data.main.temp), content: feelings});
-      f();
-    }).catch ((err) => {
-      console.log(err);
-    });
-    form.reset();
-  } else {
-    btn.classList.add('invalid');
+  getWeather(baseUrl, zipID, apiKey).then((data) => {
+    postData("/add", { temp: (data.main.temp), date: newDate, content: content }).then((data) => {
+      retrieveData();
+    })
+  });
+}
+
+/* making a get call*/
+const getWeather = async (baseUrl, zipID, apiKey) => {
+  const res = await fetch(`${baseUrl}${zipID}${apiKey}`);
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
   }
 };
 
-function tempConverter(k) {
-  if (k < (0)) {
-    return 'too low';
-  } else {
-    return (k - 273.15).toFixed(2);
-  }
-}
-
-// To get the data for the web API
-// const getWeather = async (baseURL, zip, apiKey) => {
-//   const res = await fetch(baseURL+zip+key)
-//   try {
-//     const data = await res.json();
-//     return data;
-//   }
-//   catch(err) {
-//     console.log(err);
-//   }
-// }
-
-const getWeather = async (baseURL, zip, apiKey) => {
-  const response = await fetch(`${baseURL}?q=${zip}&appid=${apiKey}`);
-  try {
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-// Function to POST data
-const postData = async (url = "", info = {}) => {
-  const res = await fetch(url, {
+/* Function to POST data */
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
     method: "POST",
-    credentials: 'same-origin',
+    credentials: "same-origin",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      temp: info.temp,
-      date: info.date,
-      content: info.content
-    }),
+    body: JSON.stringify(data),
   });
+
   try {
-    const newData = await res.json();
-    console.log(`You just saved`, newData);
+    const newData = await response.json();
     return newData;
   } catch (error) {
     console.log(error);
   }
 };
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
-// creates the date, temperature, content dynamically
-const getDate = document.getElementById("date");
-const getTemp = document.getElementById("temp");
-const getContent = document.getElementById("content");
-
-const f = async () => {
-  const response = await fetch('/all');
+const retrieveData = async () => {
+  'use strict'
+  const request = await fetch("/all");
   try {
-    const user = await response.json();
-    getDate.innerHTML = `Date: ${user[0].date}`;
-    getTemp.innerHTML = `Temperature: ${user[0].temp}`;
-    getContent.innerHTML = `Content: ${user[0].content}`;
-  } catch(err) {
-    // catches errors both in fetch and response.json
-    console.log(err);
+    const allData = await request.json();
+    console.log(allData);
+    // update the moreEntry with the necessary data
+    if (allData.date !== undefined && allData.temp !== undefined && allData.content !== undefined) {
+      document.getElementById('date').innerHTML = allData.date;
+      document.getElementById('temp').innerHTML = allData.temp + ' &#8451;';
+      document.getElementById('content').innerHTML = allData.content;
   }
-}
+  } catch (error) {
+    console.log("error", error);
+  }
+};
